1edd13c3d1186d34d8e1c39e0c6e8432ec55ce4d
nbg <nicholasnbg@gmail.com>
2017-09-11 15:13:10 +1000

    eddited side menu toggle button to form cross when opened

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
   console.log('ready');
   var sideOpen = false;
 
-  $('.open-slide').click(function(){
+
+  $('#open-slide').click(function(){
+    $(this).toggleClass('open');
     toggleSideNav();
-  })
+  });
 
   function toggleSideNav(){
     console.log('toggle side nav');
     if(!sideOpen){
-      document.getElementById('side-menu').style.width= '250px';
+      document.getElementById('side-menu').style.width= '33vw';
       sideOpen = true;
     } else{
       document.getElementById('side-menu').style.width= '0px';
